964462c5b727932fbd465d7124d98d1b6b41e570
Mario-Galindo <mgalindo9410@gmail.com>
2020-04-08 17:53:19 -0600

    edit about page

--- src/About.js
+++ src/About.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function About() {
   return (
-    <div className="App">
+    <div>
       <h1>About Page</h1>
     </div>
   );
